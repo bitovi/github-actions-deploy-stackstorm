57b215c00b34448eb0b77037ad15959610ad334f
PhillypHenning <phil8t8@gmail.com>
2022-12-08 12:33:04 -0500

    Updating the timeout on `aws_acm_certificate`

--- operations/deployment/terraform/modules/01_acm.tf
+++ operations/deployment/terraform/modules/01_acm.tf
@@ -1,13 +1,13 @@
 data "aws_acm_certificate" "issued" {
   domain   = var.domain_name
   statuses = ["ISSUED"]
-  ##depends_on = [aws_acm_certificate.ssl_certificate, time_sleep.wait_30_seconds]
+  depends_on = [time_sleep.wait_60_seconds]
 }
 
 resource "null_resource" "previous" {}
 
-resource "time_sleep" "wait_30_seconds" {
+resource "time_sleep" "wait_60_seconds" {
   depends_on = [null_resource.previous]
-  create_duration = "30s"
+  create_duration = "60s"
 }
 
