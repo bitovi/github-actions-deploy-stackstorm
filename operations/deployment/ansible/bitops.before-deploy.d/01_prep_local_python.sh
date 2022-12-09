c68e5c62ada7f8de0b15a729f28319e4c8bba7cf
PhillypHenning <phil8t8@gmail.com>
2022-12-09 15:51:42 -0500

    Updating path for ansible

--- operations/deployment/ansible/bitops.before-deploy.d/01_prep_local_python.sh
+++ operations/deployment/ansible/bitops.before-deploy.d/01_prep_local_python.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "In 01_prep_local_python.sh"
-ansible-playbook prep.yml -i $ROOT_DIR/deployment/terraform/inventory.yaml
+ansible-playbook prep.yml -i $BITOPS_ENVROOT/terraform/inventory.yaml
