d5e09a254f65a83f6a42e5b8826dd11f60a337ad
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2022-12-21 15:41:10 -0500

    debugging

--- operations/deployment/terraform/modules/10_ansible.tf
+++ operations/deployment/terraform/modules/10_ansible.tf
@@ -5,7 +5,7 @@ resource "local_sensitive_file" "private_key" {
 }
 
 resource "local_file" "ansible_inventory" {
-  filename = format("%s/%s", abspath(path.root), "inventory.yaml")
+  filename = "inventory.yaml"
   content = <<-EOT
 bitops_servers:
  hosts:
