786f6998cd7ed5ff92280a8af66b88fd912a899a
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2022-12-21 15:25:41 -0500

    try adding the inventory content direct

--- operations/deployment/terraform/modules/10_ansible.tf
+++ operations/deployment/terraform/modules/10_ansible.tf
@@ -5,13 +5,18 @@ resource "local_sensitive_file" "private_key" {
 }
 
 resource "local_file" "ansible_inventory" {
-  content = templatefile("${abspath(path.root)}/inventory.tmpl", {
-    ip          = aws_instance.server.public_ip
-    ssh_keyfile = local.filename
-    app_repo_name = var.app_repo_name
-    app_install_root = var.app_install_root
-  })
   filename = format("%s/%s", abspath(path.root), "inventory.yaml")
+  content = <<-EOT
+bitops_servers:
+ hosts:
+   ${aws_instance.server.public_ip}
+ vars:
+   ansible_ssh_user: ubuntu
+   ansible_ssh_private_key_file: ${local.filename}
+   app_repo_name: ${app_repo_name}
+   app_install_root: ${var.app_install_root}
+   ansible_python_interpreter: /usr/bin/python3
+  EOT
 }
 
 locals {
