2e73cef0ff43c3d8f9eb82f53aae232bbfaa9893
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2022-12-21 14:30:48 -0500

    update ansible clone script

--- operations/deployment/ansible/bitops.before-deploy.d/clone-ansible-st2.sh
+++ operations/deployment/ansible/bitops.before-deploy.d/clone-ansible-st2.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 set -xe
 
-PROJECT_ROOT=/Volumes/Home/Bitovi/GithubActions/github-actions-deploy-stackstorm
-
-rm -rf $PROJECT_ROOT/operations/deployment/ansible/ansible-st2
-
 echo "In clone-ansible-st2.sh"
-git clone https://github.com/StackStorm/ansible-st2 $PROJECT_ROOT/operations/deployment/ansible/ansible-st2
-cp -r $PROJECT_ROOT/operations/deployment/ansible/ansible-st2/roles/ $PROJECT_ROOT/operations/deployment/ansible/roles
-cp $PROJECT_ROOT/operations/deployment/ansible/ansible-st2/stackstorm.yml $PROJECT_ROOT/operations/deployment/ansible
 
-rm -rf $PROJECT_ROOT/operations/deployment/ansible/ansible-st2
+ANSIBLE_DIR="${ROOT_DIR}/${BITOPS_ENVIRONMENT}/ansible"
+rm -rf "${ANSIBLE_DIR}/ansible-st2"
+echo "  ANSIBLE_DIR: ${ANSIBLE_DIR}"
+
+echo "  cloning..."
+git clone https://github.com/StackStorm/ansible-st2 $ANSIBLE_DIR/ansible-st2
+echo "  cloning...Done"
+
+echo "ls contents of ANSIBLE_DIR/ansible-st2"
+ls $ANSIBLE_DIR/ansible-st2
+
+echo "  copying files from ansible-st2"
+
+echo "    $ANSIBLE_DIR/ansible-st2/roles/ -> $ANSIBLE_DIR/roles"
+cp -r $ANSIBLE_DIR/ansible-st2/roles/ $ANSIBLE_DIR/roles
+
+
+echo "    $ANSIBLE_DIR/ansible-st2/stackstorm.yml -> $ANSIBLE_DIR"
+cp $ANSIBLE_DIR/ansible-st2/stackstorm.yml $ANSIBLE_DIR
+
+echo "clean up"
+rm -rf "${ANSIBLE_DIR}/ansible-st2"
