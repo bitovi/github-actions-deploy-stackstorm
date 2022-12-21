83657ffa192791ee2e292c8f5e9d8a433bc7ac3c
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2022-12-21 16:17:54 -0500

    debugging

--- operations/deployment/ansible/bitops.before-deploy.d/clone-ansible-st2.sh
+++ operations/deployment/ansible/bitops.before-deploy.d/clone-ansible-st2.sh
@@ -3,6 +3,28 @@ set -xe
 
 echo "In clone-ansible-st2.sh"
 
+echo "ls /opt/bitops_deployment"
+ls /opt/bitops_deployment
+echo "ls /opt/bitops_deployment/deployment"
+ls /opt/bitops_deployment/deployment
+echo "ls /opt/bitops_deployment/deployment/terraform"
+ls /opt/bitops_deployment/deployment/terraform
+
+TF_DIR="${BITOPS_ENVROOT}/terraform"
+echo "TF_DIR"
+echo "$TF_DIR"
+
+echo "ls TF_DIR"
+ls "$TF_DIR"
+
+echo "ls TF_DIR/modules"
+ls "$TF_DIR/modules"
+
+echo "TF_DIR/inventory.yaml"
+cat "$TF_DIR/inventory.yaml"
+
+
+
 ANSIBLE_DIR="${BITOPS_ENVROOT}/ansible"
 rm -rf "${ANSIBLE_DIR}/ansible-st2"
 echo "  ANSIBLE_DIR: ${ANSIBLE_DIR}"
--- operations/deployment/ansible/bitops.before-deploy.d/wait-for-inventory-hosts.sh
+++ operations/deployment/ansible/bitops.before-deploy.d/wait-for-inventory-hosts.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
 
-echo "ls /opt/bitops_deployment"
-ls /opt/bitops_deployment
-echo "ls /opt/bitops_deployment/deployment"
-ls /opt/bitops_deployment/deployment
-echo "ls /opt/bitops_deployment/deployment/terraform"
-ls /opt/bitops_deployment/deployment/terraform
-
-TF_DIR="${BITOPS_ENVROOT}/terraform"
-echo "TF_DIR"
-echo "$TF_DIR"
-
-echo "ls TF_DIR"
-ls "$TF_DIR"
-
-echo "ls TF_DIR/modules"
-ls "$TF_DIR/modules"
-
-echo "TF_DIR/inventory.yaml"
-cat "$TF_DIR/inventory.yaml"
-
 echo "Waiting for instances to be ready..."
 python3 $BITOPS_TEMPDIR/_scripts/ansible/wait-for-inventory-hosts.py
