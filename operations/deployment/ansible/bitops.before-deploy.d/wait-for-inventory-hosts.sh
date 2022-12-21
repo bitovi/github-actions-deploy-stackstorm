35967a2495136703f3ec67b218a256bf9fdb91c9
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2022-12-21 14:43:39 -0500

    add some debugging for the inventory file

--- operations/deployment/ansible/bitops.before-deploy.d/wait-for-inventory-hosts.sh
+++ operations/deployment/ansible/bitops.before-deploy.d/wait-for-inventory-hosts.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-echo "I am a before ansible lifecycle script!"
+
+TF_DIR="${ROOT_DIR}/${BITOPS_ENVIRONMENT}/terraform"
+echo "TF_DIR"
+echo "$TF_DIR"
+
+echo "ls TF_DIR"
+ls "$TF_DIR"
+
+echo "TF_DIR/inventory.yaml"
+cat "$TF_DIR/inventory.yaml"
+
+echo "Waiting for instances to be ready..."
 python3 $ROOT_DIR/_scripts/ansible/wait-for-inventory-hosts.py
