7b0bf91153f9f8e4b0d1e37a0412e1f3e6e2b0fe
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2022-12-21 14:58:03 -0500

    more debugging

--- operations/deployment/ansible/bitops.before-deploy.d/wait-for-inventory-hosts.sh
+++ operations/deployment/ansible/bitops.before-deploy.d/wait-for-inventory-hosts.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "ls /opt/bitops_deployment"
+ls /opt/bitops_deployment
+echo "ls /opt/bitops_deployment/deployment"
+ls /opt/bitops_deployment/deployment
+echo "ls /opt/bitops_deployment/deployment/terraform"
+ls /opt/bitops_deployment/deployment/terraform
+
 TF_DIR="${BITOPS_ENVROOT}/terraform"
 echo "TF_DIR"
 echo "$TF_DIR"
@@ -11,4 +18,4 @@ echo "TF_DIR/inventory.yaml"
 cat "$TF_DIR/inventory.yaml"
 
 echo "Waiting for instances to be ready..."
-python3 $ROOT_DIR/_scripts/ansible/wait-for-inventory-hosts.py
+python3 $BITOPS_TEMPDIR/_scripts/ansible/wait-for-inventory-hosts.py
