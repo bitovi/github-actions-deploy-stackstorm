4f2cb0f0221a0f337497d08d0c3afe225c0e85fb
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2022-12-25 06:29:59 -0500

    ansible debugging

--- operations/deployment/ansible/bitops.before-deploy.d/clone-ansible-st2.sh
+++ operations/deployment/ansible/bitops.before-deploy.d/clone-ansible-st2.sh
@@ -3,27 +3,7 @@ set -xe
 
 echo "In clone-ansible-st2.sh"
 
-echo "ls /opt/bitops_deployment"
-ls /opt/bitops_deployment
-echo "ls /opt/bitops_deployment/deployment"
-ls /opt/bitops_deployment/deployment
-echo "ls /opt/bitops_deployment/deployment/terraform"
-ls /opt/bitops_deployment/deployment/terraform
-
 TF_DIR="${BITOPS_ENVROOT}/terraform"
-echo "TF_DIR"
-echo "$TF_DIR"
-
-echo "ls TF_DIR"
-ls "$TF_DIR"
-
-echo "ls TF_DIR/modules"
-ls "$TF_DIR/modules"
-
-echo "TF_DIR/inventory.yaml"
-cat "$TF_DIR/inventory.yaml"
-
-
 
 ANSIBLE_DIR="${BITOPS_ENVROOT}/ansible"
 rm -rf "${ANSIBLE_DIR}/ansible-st2"
@@ -38,13 +18,17 @@ ls $ANSIBLE_DIR/ansible-st2
 
 echo "  copying files from ansible-st2"
 
-echo "ls ANSIBLE_DIR/roles ($ANSIBLE_DIR/roles) before"
+echo "ls ANSIBLE_DIR before"
+ls $ANSIBLE_DIR
+echo "ls ANSIBLE_DIR/roles before"
 ls $ANSIBLE_DIR/roles
 
 echo "    $ANSIBLE_DIR/ansible-st2/roles/ -> $ANSIBLE_DIR/roles"
 cp -r $ANSIBLE_DIR/ansible-st2/roles/ $ANSIBLE_DIR/roles
 
-echo "ls ANSIBLE_DIR/roles ($ANSIBLE_DIR/roles) after"
+echo "ls ANSIBLE_DIR after"
+ls $ANSIBLE_DIR
+echo "ls ANSIBLE_DIR/roles after"
 ls $ANSIBLE_DIR/roles
 
 
