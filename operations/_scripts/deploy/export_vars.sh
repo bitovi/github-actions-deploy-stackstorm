62cc300932527f7e74ec24a87f9490e183a60743
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2022-12-29 09:51:19 -0500

    debug BO_OUT

--- operations/_scripts/deploy/export_vars.sh
+++ operations/_scripts/deploy/export_vars.sh
@@ -4,5 +4,9 @@
 BO_OUT="$GITHUB_ACTION_PATH/operations/bo-out.env"
 
 if [ -f $BO_OUT ]; then
+  echo "BO_OUT ($BO_OUT)"
+  cat $BO_OUT
   cat $BO_OUT >> $GITHUB_OUTPUT
+else
+  echo "BO_OUT not a file ($BO_OUT)"
 fi
