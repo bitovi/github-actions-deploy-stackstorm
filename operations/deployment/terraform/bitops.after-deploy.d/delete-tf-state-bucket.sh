44301a685c6483348166330da044310089dac809
PhillypHenning <phil8t8@gmail.com>
2023-01-06 11:04:02 -0500

    Destroy bucket only when TERRAFORM_DESTROY & TF_STATE_BUCKET_DESTROY = true

--- operations/deployment/terraform/bitops.after-deploy.d/delete-tf-state-bucket.sh
+++ operations/deployment/terraform/bitops.after-deploy.d/delete-tf-state-bucket.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TF_STATE_BUCKET_DESTROY = true ]] ; then
+if [[ $TERRAFORM_DESTROY = true ]] && [[ $TF_STATE_BUCKET_DESTROY = true ]]; then
   echo "Destroying S3 buket --> $TF_STATE_BUCKET"
   aws s3 rb s3://$TF_STATE_BUCKET --force
 fi
