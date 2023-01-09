02a11f833f3898a7658f3647292601b6d16998da
PhillypHenning <phil8t8@gmail.com>
2023-01-09 15:52:42 -0500

    Updating logic around st2.conf copy and delete

--- operations/_scripts/generate/ansible/generate_st2_conf.sh
+++ operations/_scripts/generate/ansible/generate_st2_conf.sh
@@ -2,5 +2,8 @@
 echo "In generate_st2_conf.sh"
 
 if [[ -n $ST2_CONF_PATH ]]; then
-    cp $GITHUB_WORKSPACE/$ST2_CONF_PATH $GITHUB_ACTION_PATH/operations/deployment/ansible/vars/st2.conf
+    cp $GITHUB_WORKSPACE/$ST2_CONF_PATH $GITHUB_ACTION_PATH/operations/deployment/ansible/vars/
 fi
+
+# Delete the default?
+rm $GITHUB_ACTION_PATH/operations/deployment/ansible/vars/st2.yaml
