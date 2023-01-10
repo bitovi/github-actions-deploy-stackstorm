75b586049cb454a4999d5fe2129e9baa2e007a53
LeoDiazL <leo@bitovi.com>
2023-01-10 13:39:30 -0300

    Adding warning per Mick suggestion

--- operations/deployment/terraform/bitops.after-deploy.d/delete-tf-state-bucket.sh
+++ operations/deployment/terraform/bitops.after-deploy.d/delete-tf-state-bucket.sh
@@ -3,4 +3,8 @@
 if [[ $TERRAFORM_DESTROY = true ]] && [[ $TF_STATE_BUCKET_DESTROY = true ]]; then
   echo "Destroying S3 buket --> $TF_STATE_BUCKET"
   aws s3 rb s3://$TF_STATE_BUCKET --force
+else 
+  if [[ $TERRAFORM_DESTROY = true ]] && [[ $TF_STATE_BUCKET_DESTROY = true ]]; then
+    echo "TF_STATE_BUCKET_DESTROY set to true, but TERRAFORM_DESTROY wasn't. Not destroying the state bucket ($TF_STATE_BUCKET)"
+  fi
 fi
