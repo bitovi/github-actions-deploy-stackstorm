c3ef4fbaf080526ea3994cc0365b8582c6f3962a
PhillypHenning <phil8t8@gmail.com>
2023-01-11 10:29:57 -0500

    Updating action input name, description and use

--- operations/_scripts/generate/ansible/generate_st2_conf.sh
+++ operations/_scripts/generate/ansible/generate_st2_conf.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 echo "In generate_st2_conf.sh"
 
-if [[ -n $ST2_CONF_PATH ]]; then
+if [[ -n $ST2_ANSIBLE_VARS_FILE ]]; then
     cp $GITHUB_WORKSPACE/$ST2_CONF_PATH $GITHUB_ACTION_PATH/operations/deployment/ansible/vars/
 fi
-
-# Delete the default?
-rm $GITHUB_ACTION_PATH/operations/deployment/ansible/vars/st2.yaml
