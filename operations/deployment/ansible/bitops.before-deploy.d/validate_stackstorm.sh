951bcb765363dc97bf0a80d1c720990d3705ee06
PhillypHenning <phil8t8@gmail.com>
2023-01-18 16:27:40 -0500

    setting default pack

--- operations/deployment/ansible/bitops.before-deploy.d/validate_stackstorm.sh
+++ operations/deployment/ansible/bitops.before-deploy.d/validate_stackstorm.sh
@@ -5,5 +5,5 @@ set -ex
 # This file is needed as ST2_PACKS does have an action default however it is possible for a user to set this effectively null "" which will cause issues during the stackstorm installation.
 
 if [[ $ST2_PACKS == "" ]]; then
-    unset ST2_PACKS
+    export ST2_PACKS="st2"
 fi
