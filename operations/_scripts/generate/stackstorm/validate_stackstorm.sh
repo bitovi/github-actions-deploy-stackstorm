76480c5c7e2e9e468b2419a69216e7faf47d85b8
PhillypHenning <phil8t8@gmail.com>
2023-01-18 14:21:58 -0500

    Adding validate_stackstorm.sh

---
+++ operations/_scripts/generate/stackstorm/validate_stackstorm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+# This file is needed as ST2_PACKS does have an action default however it is possible for a user to set this effectively null "" which will cause issues during the stackstorm installation.
+
+if [[ $ST2_PACKS == "" ]]; then
+    export ST2_PACKS='st2'
+fi
