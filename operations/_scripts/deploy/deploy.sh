90bc1281498d1a50d262ce56d5c6b405a49f6b61
Eugen <eugen@bitovi.com>
2023-02-20 23:22:43 +0000

    Switch to BitOps v2.4.0 release

--- operations/_scripts/deploy/deploy.sh
+++ operations/_scripts/deploy/deploy.sh
@@ -65,4 +65,4 @@ docker run --rm --name bitops \
 -e ST2_PACKS="${ST2_PACKS}" \
 ${DOCKER_EXTRA_ARGS} \
 -v $(echo $GITHUB_ACTION_PATH)/operations:/opt/bitops_deployment \
-bitovi/bitops:2.3.0
+bitovi/bitops:2.4.0
