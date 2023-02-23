98c31b1159f4833346c4eb395c6a910047abad17
Eugen <eugen@bitovi.com>
2023-02-23 23:29:33 +0000

    Add pipefail to to bucket identifiers script

--- operations/_scripts/generate/generate_buckets_identifiers.sh
+++ operations/_scripts/generate/generate_buckets_identifiers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -eo pipefail
 
 GITHUB_IDENTIFIER="$(echo $($GITHUB_ACTION_PATH/operations/_scripts/generate/generate_identifier.sh) | tr '[:upper:]' '[:lower:]' | tr '_' '-' | tr '/' '.' )"
 
